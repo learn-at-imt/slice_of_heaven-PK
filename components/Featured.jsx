fc9247c9fd76afe274f30bee7a1ea4611ad86a64
Aaron Addae <addaeaaron28@gmail.com>
2022-09-19 20:13:26 +0000

    created the navbar

---
+++ components/Featured.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Image from 'next/image';
+import styles from "../styles/Featured.module.css";
+
+const Featured = () => {
+  return (
+    <div>Featured</div>
+  )
+}
+
+export default Featured
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Featured from '../components/Featured';
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-
       <Head>
         <title>Slice Of Heaven</title>
         <meta name="description" content="best pizza shop in town" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      homepage
-
+      <Featured/>
     </div>
   );
 }
